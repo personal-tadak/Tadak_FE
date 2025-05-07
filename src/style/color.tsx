0ebe5afa1e9b950a88ad9dbaa49383a0c34fa3cb
seeum0507 <kwonstella0507@gmail.com>
2025-05-07 23:39:32 +0900

    chore : initial setting

---
+++ src/style/color.tsx
@@ -0,0 +1,19 @@
+export const color = {
+  White: "#FFFFFF",
+
+  Black: "#000000",
+
+  Bg: "#FFF8F7",
+
+  Green: "#4DC133",
+
+  Yellow: ["#FFD773", "#F5BB29", "#FBFFC9"],
+
+  Blue: ["#4C4CC7", "#C9E9FF"],
+
+  Red: ["#C74C4C", "#FFC9C9"],
+
+  Purple: "#B09AFF",
+
+  Gray: ["#757575", "#F9F9F9", "#9D9D9D", "#757575", "#585858"],
+};
